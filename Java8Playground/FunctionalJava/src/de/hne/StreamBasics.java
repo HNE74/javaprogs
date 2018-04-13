3c84985d733cb8f1eaae4a44598f9c3376bb67b5
Nolte, Heiko <heiko.nolte@talanx.com>
2018-04-13 14:36:58 +0200

    Mehr Stream Experimente

--- Java8Playground/FunctionalJava/src/de/hne/StreamBasics.java
+++ Java8Playground/FunctionalJava/src/de/hne/StreamBasics.java
@@ -1,10 +1,12 @@
 package de.hne;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.function.IntPredicate;
 import java.util.function.Predicate;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
@@ -27,12 +29,11 @@ public class StreamBasics {
 		System.out.println("\nInteger Filterung:");		
 		IntStream integerStream = IntStream.range(20, 40);
 		final IntPredicate divByThreePred = val -> val % 3 == 0;
-		List<Integer> divByThreeList = integerStream.filter(divByThreePred).boxed().collect(Collectors.toList());
-		divByThreeList.forEach(System.out::println);
+		integerStream.filter(divByThreePred).boxed().forEach(System.out::println);
 		
 		System.out.println("\nMapping:");
 		integerStream = IntStream.range(0, 10);
-		integerStream.map(x -> x * 2).boxed().collect(Collectors.toList()).forEach(System.out::println);
+		integerStream.map(x -> x * 2).boxed().forEach(System.out::println);
 		
 		System.out.println("\nFlat Mapping von verschachtelten Streams:");
 		final Stream<String> senctencesStream = Arrays.asList( "Dies ist ein Java 8 Beispiel.",
@@ -45,6 +46,32 @@ public class StreamBasics {
 		final Stream<String> txtStream = Arrays.stream(new String[] { "ab4grth", "55itgg", "hgiwer6", "4HgJ55" });
 		txtStream.map(txt -> txt.toUpperCase()).peek(System.out::println).map(txt -> txt.replaceAll("[0-9]", "")).
 								collect(Collectors.toList()).forEach(System.out::println);
+		
+		System.out.println("\nParalleler Stream mit Zustand unsynchronisiert:");		
+	    for (int i = 0; i < 5; i++) {
+	        Set<Integer> seen = new HashSet<>();       	    	
+            System.out.println(sumUpUniqueNumbers(seen));
+	    }	    
+		
+		System.out.println("\nParalleler Stream mit Zustand synchronisiert:");		
+	    for (int i = 0; i < 5; i++) {
+	        Set<Integer> seen = Collections.synchronizedSet(new HashSet<>());       	    	
+            System.out.println(sumUpUniqueNumbers(seen));
+	    }	    
+	}
+	
+	private int sumUpUniqueNumbers(Set<Integer> seen) {
+        IntStream stream = IntStream.of(1, 2, 1, 2, 3, 4, 4, 5); 
+        int sum = stream.parallel().map(
+            //stateful behavioral parameter.
+            e -> {
+                  if (seen.add(e))
+                      return e;
+                  else
+                      return 0;
+              }).sum();	
+        
+        return sum;
 	}
 
 }
