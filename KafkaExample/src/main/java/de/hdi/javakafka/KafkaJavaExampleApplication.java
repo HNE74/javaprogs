d22846233ddd05469386a9ea065fc7fc59442a75
HNE74 <30633070+HNE74@users.noreply.github.com>
2020-12-16 14:28:47 +0100

    Infos how to set up sandbox

--- KafkaExample/src/main/java/de/hdi/javakafka/KafkaJavaExampleApplication.java
+++ KafkaExample/src/main/java/de/hdi/javakafka/KafkaJavaExampleApplication.java
@@ -3,6 +3,17 @@ package de.hdi.javakafka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Kafka Spring Boot sandbox app.
+ * 1) Before starting install Kafka to your local machine: https://kafka.apache.org/
+ * 2) Go to directory %KAFKA_HOME%/bin/windows to access the batch scripts.
+ * 3) Start zookeeper: zookeeper-server-start.bat ..\\..\\config\\zookeeper.properties
+ * 4) Start the Kafka broker: kafka-server-start.bat ..\\..\\config\\server.properties
+ * 5) Create a topic: kafka-topics.bat --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic myTopic
+ * 
+ * Send and receive messages by calling the services provided by the REST-controller class.
+ * @author 057530
+ */
 @SpringBootApplication
 public class KafkaJavaExampleApplication {
 
