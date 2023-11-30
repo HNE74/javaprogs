6d146456a80956f74598a495cd23b885cf05d347
Heiko Nolte <heiko.nolte@hdi.de>
2023-11-30 16:29:29 +0100

    Add readme and license

---
+++ springaieval/readme.md
@@ -0,0 +1,4 @@
+Evaluation of Java Spektrum 06/2023 article "Smartere Anwendungen mit Spring AI" by Michael Hunger.
+
+JSON-Resource from Github Repo:
+https://github.com/Azure-Samples/spring-ai-azure-workshop/blob/main/src/main/resources/data/bikes.json
--- springaieval/src/main/java/de/hne/springaieval/SpringAIEvaluationApplication.java
+++ springaieval/src/main/java/de/hne/springaieval/SpringAIEvaluationApplication.java
@@ -2,7 +2,6 @@ package de.hne.springaieval;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class SpringAIEvaluationApplication {
 	public static void main(String[] args) {
